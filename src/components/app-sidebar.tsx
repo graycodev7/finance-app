bceb4314eaf9ba57ac732dbb1a2bd38a2b500864
Andres <andres.st1998@gmail.com>
2025-08-11 17:51:53 -0500

    Solucionando colores en sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -58,7 +58,7 @@ export function AppSidebar() {
       {/* Sidebar */}
       <aside
         className={cn(
-          "fixed left-0 top-0 z-40 h-full w-64 modern-sidebar transition-transform duration-300 ease-in-out md:relative md:translate-x-0",
+          "fixed left-0 top-0 z-40 h-full w-64 bg-white border-r border-slate-200 shadow-xl transition-transform duration-300 ease-in-out md:relative md:translate-x-0 md:shadow-none",
           mobileMenuOpen ? "translate-x-0" : "-translate-x-full md:translate-x-0"
         )}
       >
